97ab2cedf218f0bddd3e72bd116adfc671e70dee
taohansens <taohansens@gmail.com>
2022-05-20 17:16:23 -0300

    Project Unity Alura

---
+++ Assets/Scripts/GeradorZumbis.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorZumbis : MonoBehaviour {
+
+    public GameObject Zumbi;
+    private float contadorTempo = 0;
+    public float TempoGerarZumbi = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        contadorTempo += Time.deltaTime;
+
+        if(contadorTempo >= TempoGerarZumbi)
+        {
+            Instantiate(Zumbi, transform.position, transform.rotation);
+            contadorTempo = 0;
+        }
+
+    }
+}
